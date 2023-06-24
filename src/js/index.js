8cabf5c869b27feee9d371e8360c4c6efaef0033
christopher garcia <71852007+christopher-garcia1@users.noreply.github.com>
2023-06-24 01:08:18 +0000

    done

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,49 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
+import PropType from "prop-types"
+
+
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
 
+//import your own components
+
+
+
+const SimpleCounter = (props)=>{
+    return(
+        <div className="Counter">
+            <div className="clock"><FontAwesomeIcon icon={faClock} /></div>
+            <div className="four">{props.countFour%10}</div>
+            <div className="three">{props.countThree%10}</div>
+            <div className="two">{props.countTwo%10}</div>
+            <div className="one">{props.countOne%10}</div>
+        </div>
+    )
+}
+
+ SimpleCounter.propTypes = {
+   countOne: PropType.number,
+   countTwo: PropType.number,
+   countThree: PropType.number,
+   countFour: PropType.number
+}
+
+let counter = 0
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+setInterval(function(){
+   const four = Math.floor(counter/1000)
+   const three = Math.floor(counter/100)
+   const two = Math.floor(counter/10)
+   const one = Math.floor(counter/1)
+   counter++
+   console.log(one, two, three, four);
+
+   ReactDOM.render(<SimpleCounter countOne={one} countTwo={two} countThree={three} countFour={four}/>, document.querySelector("#app"));
+
+},1000) 
